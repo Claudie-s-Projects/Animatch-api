fdd0d1c12b5bbee4ed69c4149db67c44d197d1cc
Claudia C18 🦇 <claudia.capuana@gmail.com>
2026-06-03 16:29:45 +0200

    evaluation defi projet

--- src/scraping/scraping.controller.ts
+++ src/scraping/scraping.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, HttpCode, Post } from '@nestjs/common';
+import { Controller, HttpCode, Post, UseGuards } from '@nestjs/common';
 import { ScrapingService } from './scraping.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('scraping')
 export class ScrapingController {
@@ -7,6 +8,9 @@ export class ScrapingController {
 
   @Post('sync')
   @HttpCode(200)
+  @UseGuards(AuthGuard('jwt'))
+// j'ai utilisé le gard pour empêcher l'abus de scrapping manuel (j'ai suivi l'ennoncé qui me conseillait de réutiliser la méthode déjà présente dans mon code mais celà signifie que les utilisateurs enregistrés pourront lancer un scrapping manuel, pas la solution la plus optimale)
+
   sync() {
     return this.scrapingService.sync();
   }
